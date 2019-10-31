4eda274c91099534f848008bf59d050d78ccf70c
Leonardo Scorza <contato@escoladejavascript.com>
2019-10-31 14:18:11 -0300

    Aula - Primeira rota com express

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Minha lista de tarefas :)</h1>');
+})
+
+app.listen(3000, () => {
+  console.log('Servidor foi iniciado');
+})
