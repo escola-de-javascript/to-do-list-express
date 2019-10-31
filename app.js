0ae1d0dbae82781d2e001d23aa354ea016d53891
Leonardo Scorza <contato@escoladejavascript.com>
2019-10-31 15:09:37 -0300

    Aula - Middleware

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+const log = (req, res, next) => {
+  console.log(req.body);
+  console.log(`Data: ${Date.now()}`);
+  next();
+}
+
+app.use(log);
+
 app.get('/', (req, res) => {
   res.send('<h1>Minha lista de tarefas :)</h1>');
 })
 
 app.get('/json', (req, res) => {
-  res.json({title: 'Tarefa X', done: true});
+  res.json({title: 'Tarefa X', done: true});  
 })
 
 app.listen(3000, () => {
