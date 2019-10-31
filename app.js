768671600b1518d3152702f857995e71cf5e0751
Leonardo Scorza <contato@escoladejavascript.com>
2019-10-31 14:25:52 -0300

    Aula - Resposta JSON

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('<h1>Minha lista de tarefas :)</h1>');
 })
 
+app.get('/json', (req, res) => {
+  res.json({title: 'Tarefa X', done: true});
+})
+
 app.listen(3000, () => {
   console.log('Servidor foi iniciado');
 })
