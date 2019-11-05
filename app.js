0b16b25f39ab3b178558eb19e252b9e7f4da354d
Leonardo Scorza <contato@escoladejavascript.com>
2019-11-05 09:51:09 -0300

    EJS e configuração express

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const path = require('path');
+
 const checkListRouter = require('./src/routes/checklist');
+const rootRouter = require('./src/routes/index');
+
 require('./config/database');
 
 
 const app = express();
 app.use(express.json());
 
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
+app.use('/', rootRouter);
 app.use('/checklists', checkListRouter);
 
 app.listen(3000, () => {
