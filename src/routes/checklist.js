8dafed48976cf3a56f125c306aa2db1f4ec9d1f1
Leonardo Scorza <contato@escoladejavascript.com>
2019-11-05 10:50:25 -0300

    Aula - Dados através das Rotas

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -7,9 +7,9 @@ const Checklist = require('../models/checklist');
 router.get('/', async (req, res) => {
   try {
     let checklists = await Checklist.find({});
-    res.status(200).json(checklists);
+    res.status(200).render('checklists/index', { checklists: checklists})
   } catch (error) {
-    res.status(500).json(error)
+    res.status(200).render('pages/error', {error: 'Erro ao exibir as Listas'});
   }
 })
 
@@ -27,9 +27,9 @@ router.post('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     let checklist = await Checklist.findById(req.params.id);
-    res.status(200).json(checklist);
+    res.status(200).render('checklists/show', { checklist: checklist})
   } catch (error) {
-    res.status(422).json(error);
+    res.status(200).render('pages/error', {error: 'Erro ao exibir as Listas de tarefas'});
   }
 })
 
