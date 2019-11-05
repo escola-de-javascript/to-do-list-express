39c3cd9bc145e4ff4478aaa0796270e19b30d53a
Leonardo Scorza <contato@escoladejavascript.com>
2019-11-05 10:03:36 -0300

    Navegando no App

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ require('./config/database');
 const app = express();
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 
